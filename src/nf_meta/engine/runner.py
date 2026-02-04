b7b8c238866029ac975bdc7fee5895915b186f8e
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-02-04 14:59:16 +0100

    Rename module to graph. Add dummy events, history, and layout

--- src/nf_meta/engine/runner.py
+++ src/nf_meta/engine/runner.py
@@ -1,8 +1,19 @@
+import logging
 from enum import StrEnum
 
+from src.nf_meta.engine.graph import MetaworkflowGraph
+
+logger = logging.getLogger()
 
 class Runners(StrEnum):
     PYTHON = "python"
     DAISY_CHAIN = "daisy_chain"
     NEXTFLOW_MONO = "nextflow_mono"
 
+
+# ToDO: Receive a Metawf_graph
+def run(g: MetaworkflowGraph, runner: Runners) -> None:
+    logger.info("Started runner")
+
+
+# TODO: Implement with protocols

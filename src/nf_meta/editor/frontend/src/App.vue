2e38cad62eef609179b9aaee8d1dc870b94ee2af
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-02-04 14:46:26 +0100

    Adds VueFlow frontend

---
+++ src/nf_meta/editor/frontend/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { VueFlow, type Node } from '@vue-flow/core'
+import { Background } from '@vue-flow/background'
+import { Controls } from '@vue-flow/controls'
+
+const nodes = ref<Node[]>([
+  {
+    id: '1',
+    position: { x: 250, y: 150 },
+    data: { label: 'Hello Vue Flow' },
+    type: 'default',
+  },
+])
+</script>
+
+<template>
+  <div style="width: 100vw; height: 100vh;">
+    <VueFlow :nodes="nodes" fit-view>
+      <Background />
+      <Controls />
+    </VueFlow>
+  </div>
+</template>
---
+++ src/nf_meta/editor/frontend/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '@vue-flow/core/dist/style.css'
+
+createApp(App).mount('#app')

2f1d5609cec69326acc4d44b4e310a1534506049
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-02-05 21:18:54 +0100

    Remove unused import

--- src/nf_meta/engine/events.py
+++ src/nf_meta/engine/events.py
@@ -2,8 +2,6 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import StrEnum
 
-from pydantic import BaseModel
-
 from .graph import MetaworkflowGraph
 from .models import Workflow, Transition
 

d7cd635311f07709a4d9e6b08239cb6b0fe879f2
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-02-07 00:11:22 +0100

    add typing

--- src/nf_meta/engine/history.py
+++ src/nf_meta/engine/history.py
@@ -4,20 +4,20 @@ from .graph import MetaworkflowGraph
 
 class History:
     def __init__(self):
-        self.undo_stack = []
-        self.redo_stack = []
+        self.undo_stack: list[Command] = []
+        self.redo_stack: list[Command] = []
 
     def execute(self, command: Command, graph: MetaworkflowGraph):
         command.apply(graph)
         self.undo_stack.append(command)
         self.redo_stack.clear()
 
-    def undo(self, graph):
+    def undo(self, graph: MetaworkflowGraph):
         cmd = self.undo_stack.pop()
         cmd.undo(graph)
         self.redo_stack.append(cmd)
 
-    def redo(self, graph):
+    def redo(self, graph: MetaworkflowGraph):
         cmd = self.redo_stack.pop()
         cmd.apply(graph)
         self.undo_stack.append(cmd)

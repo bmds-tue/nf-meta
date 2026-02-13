c9455f7d1d60900f092b2d8708e9f7141a8895ed
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-02-13 12:26:33 +0100

    Refactor use of props. Use ts to fix import issues. styling

--- src/nf_meta/editor/frontend/src/components/WorkflowNode.vue
+++ src/nf_meta/editor/frontend/src/components/WorkflowNode.vue
@@ -1,10 +1,16 @@
-<script setup>
+<script setup lang="ts">
 import { Position, Handle } from '@vue-flow/core'
 import { computed } from 'vue'
-import { useLayout } from '../LayoutGraph.vue'
+import { useLayout } from '../layout_graph.ts'
+
+const props = defineProps({
+        label: String,
+        layout: String,
+        data: Object
+    })
 
 const { layoutOptions } = useLayout()
-const props = defineProps(["label", "layout", "data"])
+
 const horizLayout = computed(() => {
     console.log("props.layout", props.layout)
     console.log("Layouts.horizontal", layoutOptions.horizontal)
@@ -15,52 +21,48 @@ const horizLayout = computed(() => {
 <template>
   <div 
         class="workflow-node"
-        :class="{'workflow-node-nfcore' : data?.is_nfcore} ">
+        :class="{'workflow-node-nfcore' : props.data?.is_nfcore} ">
     <Handle class="workflow-node-handle" 
             :class="{'handle-horiz' : horizLayout}"
             type="target" 
             :position="horizLayout ? Position.Left : Position.Top"
             />
-    <div class="workflow-node-label">{{ label }}</div>
-    <div> {{ data.pipeline_location }} </div>
+    <div>{{ props.label }}</div>
+    <div> {{ props.data?.pipeline_location }} </div>
     <Handle class="workflow-node-handle"
             :class="{'handle-horiz' : horizLayout}"
             type="source" 
             :position="horizLayout ? Position.Right : Position.Bottom"
             />
   </div>
+
 </template>
 
 <style scoped>
 :global(.workflow-node) {
     padding: 10px;
-    color: #fff;
-    background: #9CA8B3;
+    color: #303030;
+    background: #efefef;
     border-width: 2px;
     border-style: solid;
-    border-radius: 4px;
-    border-color: #ca7820;
+    border-radius: 10px;
+    border-color: #925819;
 }
 :global(.workflow-node:hover) {
-    box-shadow:0 0 0 2px #2563eb;
-    transition:box-shadow .2s
+    box-shadow:0 0 0 2px #6c6c6c80;
+    transition:box-shadow .2s;
 }
 :global(.workflow-node-nfcore) {
     border-color: #1a9655;
 }
 :global(.workflow-node-handle) {
     background: #2d3f46;
-    width: 20px;
-    height: 3px;
-    border-radius: 1px;
+    width: 35px;
+    height: 7px;
+    border-radius: 10px;
 }
 :global(.handle-horiz) {
-    width: 3px;
-    height: 10px;
-}
-
-:global(.workflow-node-label) {
-    width: 100%;
-    justify-content: center
+    width: 7px;
+    height: 35px;
 }
 </style>

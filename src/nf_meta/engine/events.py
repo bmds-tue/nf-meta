307b072d494a5ea1729646dc6afe3dc1ce52b140
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-02-18 16:35:49 +0100

    Add abstract protocol for a graph event handler to avoid circular import

--- src/nf_meta/engine/events.py
+++ src/nf_meta/engine/events.py
@@ -2,7 +2,6 @@ from typing import Protocol
 from dataclasses import dataclass
 from enum import StrEnum
 
-from .graph import MetaworkflowGraph
 from .models import Workflow, Transition
 
 
@@ -12,6 +11,21 @@ class EventTypes(StrEnum):
     RemoveWorkflow = "remove_workflow"
     AddTransition = "add_transition"
     RemoveTransition = "remove_transition"
+
+
+# Event Handler
+class GraphEventHandler(Protocol):
+    def add_workflow(self, w: Workflow) -> None: ...
+
+    def remove_workflow(self, w: Workflow) -> None: ...
+
+    def update_workflow(self, w: Workflow) -> None: ...
+
+    def add_transition(self, t: Transition) -> None: ...
+
+    def remove_transition(self, t: Transition) -> None: ...
+
+    def update_transition(self, t: Transition) -> None: ...
 
 
 # ------------------------------------------
@@ -58,19 +72,19 @@ class TransitionAltered:
 
 class Command(Protocol):
 
-    def apply(self, graph: MetaworkflowGraph) -> None: ...
+    def apply(self, graph: GraphEventHandler) -> None: ...
 
-    def undo(self, graph: MetaworkflowGraph) -> None: ...
+    def undo(self, graph: GraphEventHandler) -> None: ...
 
 
 @dataclass(frozen=True)
 class AddWorkflow:
     workflow: Workflow
 
-    def apply(self, g: MetaworkflowGraph):
+    def apply(self, g: GraphEventHandler):
         g.add_workflow(self.workflow)
 
-    def undo(self, g: MetaworkflowGraph):
+    def undo(self, g: GraphEventHandler):
         g.remove_workflow(self.workflow)
 
 
@@ -79,10 +93,10 @@ class RemoveWorkflow:
     # TODO: Think about removing single nodes (Command design) vs whole subgraphs (Memento / subgraph caching)
     workflow: Workflow
 
-    def apply(self, g: MetaworkflowGraph):
+    def apply(self, g: GraphEventHandler):
         g.remove_workflow(self.workflow)
 
-    def undo(self, g: MetaworkflowGraph):
+    def undo(self, g: GraphEventHandler):
         g.remove_workflow(self.workflow)
 
 
@@ -90,10 +104,10 @@ class RemoveWorkflow:
 class AddTransition:
     transition: Transition
 
-    def apply(self, g: MetaworkflowGraph):
+    def apply(self, g: GraphEventHandler):
         g.add_transition(self.transition)
 
-    def undo(self, g: MetaworkflowGraph):
+    def undo(self, g: GraphEventHandler):
         g.remove_workflow(self.transition)
 
 
@@ -101,10 +115,10 @@ class AddTransition:
 class RemoveTransition:
     transition: Transition
 
-    def apply(self, g: MetaworkflowGraph):
+    def apply(self, g: GraphEventHandler):
         g.remove_workflow(self.transition)
 
-    def undo(self, g: MetaworkflowGraph):
+    def undo(self, g: GraphEventHandler):
         g.add_transition(self.transition)
 
 
@@ -117,10 +131,10 @@ class EditWorkflow:
         if self.new_workflow.id != self.old_workflow.id:
             raise ValueError("Id must not be edited!")
 
-    def apply(self, g: MetaworkflowGraph):
+    def apply(self, g: GraphEventHandler):
         g.update_workflow(self.new_workflow)
 
-    def undo(self, g: MetaworkflowGraph):
+    def undo(self, g: GraphEventHandler):
         g.update_workflow(self.old_workflow)
 
 
@@ -133,8 +147,8 @@ class EditTransition:
         if self.new_transition.id != self.old_transition.id:
             raise ValueError("Id must not be edited!")
 
-    def apply(self, g: MetaworkflowGraph):
+    def apply(self, g: GraphEventHandler):
         g.update_workflow(self.new_transition)
 
-    def undo(self, g: MetaworkflowGraph):
+    def undo(self, g: GraphEventHandler):
         g.update_workflow(self.old_transition)

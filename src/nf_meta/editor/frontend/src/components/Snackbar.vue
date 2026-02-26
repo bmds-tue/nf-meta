fe146b8218401932144e1809253cc6bf59440b6e
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-02-26 16:51:11 +0100

    Add snackbar component

---
+++ src/nf_meta/editor/frontend/src/components/Snackbar.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-snackbar-queue v-model="messageStore.queue" closable="true"></v-snackbar-queue>
+</template>
+
+<script setup lang="ts">
+import { useMessageStore } from '../store';    
+const messageStore = useMessageStore()
+</script>

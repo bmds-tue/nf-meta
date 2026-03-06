914265c6e4e7a0c7d67bf960728334c42158c58f
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-03-06 15:01:29 +0100

    Catch Yaml errors

--- src/nf_meta/editor/frontend/src/components/YamlEditor.vue
+++ src/nf_meta/editor/frontend/src/components/YamlEditor.vue
@@ -2,7 +2,7 @@
 import { ref, computed } from "vue"
 import { Codemirror } from "vue-codemirror"
 import { yaml as langyaml } from "@codemirror/lang-yaml"
-import YAML from "yaml"
+import YAML, { YAMLParseError } from "yaml"
 import { EditorView } from "@codemirror/view"
 import { oneDark } from "@codemirror/theme-one-dark"
 import { useGraphStore } from "../store"
@@ -13,6 +13,7 @@ const props = defineProps<{nodeData: APINodeData}>()
 
 const paramsString = YAML.stringify(props.nodeData.params)
 const code = ref(paramsString || "")
+const error = ref<string>("")
 
 const fullHeightTheme = EditorView.theme({
   "&": { height: "99%" },
@@ -20,14 +21,26 @@ const fullHeightTheme = EditorView.theme({
 })
 
 function save() {
-    const params = YAML.parse(code.value)
-    const nodeUpdate = props.nodeData
-    nodeUpdate.params = params
-    graphStore.saveNode(nodeUpdate)
+    try {
+        error.value = ""
+        const params = YAML.parse(code.value)
+        const nodeUpdate = props.nodeData
+        nodeUpdate.params = params
+        graphStore.saveNode(nodeUpdate)
+    } catch (err: unknown) {
+        if (err instanceof YAMLParseError) {
+            error.value = err.message
+        } else {
+            console.log(err)
+        }
+    }
 }
 </script>
 
 <template>
+    <p v-if="error" style="color: rgb(var(--v-theme-error))">
+        {{ error }}
+    </p>    
     <Codemirror
         v-model="code"
         :extensions="[langyaml(), fullHeightTheme, oneDark]"

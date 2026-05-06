86308235f69b7f7f3aa5fa15bd1bda2614c3e4cc
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-05-06 13:01:15 +0200

    Remove unused functions

--- src/nf_meta/core/nf_core_utils.py
+++ src/nf_meta/core/nf_core_utils.py
@@ -4,28 +4,10 @@ import logging
 from dataclasses import dataclass
 
 import requests
-import networkx as nx
-from networkx.readwrite import json_graph
 
 
 logger = logging.getLogger()
 
-
-def save_graph_to_file(graph: nx.DiGraph, file: str):
-    data = json_graph.adjacency_data(graph)
-    s = json.dumps(data, indent=2)
-
-    with open(file, "w") as f:
-        f.write(s)
-
-def load_graph_from_file(file: str):
-    
-    with open(file, "r") as f:
-        data = json.loads(f.read())
-    
-    g = json_graph.adjacency_graph(data)
-
-    return g
 
 @functools.cache
 def get_nfcore_pipelines() -> list[dict]:

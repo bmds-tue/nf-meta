1b5d790bb79fb522360465d3325fa9ee69dc59f7
Julian Flesch <julian.flesch@qbic.uni-tuebingen.de>
2026-06-03 11:30:31 +0200

    Set vite port explicitly

--- src/nf_meta/editor/frontend/vite.config.ts
+++ src/nf_meta/editor/frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// Proxy only used in development context, when vite is accessed directly
 export default defineConfig({
   plugins: [vue()],
   server: {
+    port: 5173,
     proxy: {
       "/api": {
         target: "http://localhost:8080",
